641e6739e42eca25fb76cd2ff82422112dda0afa
shadow3x3x3 <shadow3x3x3@gmail.com>
2017-01-03 13:38:37 +0800

    rewrite intake reader, return intake[:age_class][:food_class]

--- src/reader.rb
+++ src/reader.rb
@@ -1,75 +1,44 @@
 require 'csv'
 
 module Reader
-  def self.base
-    base_data = {
-      :milk       => [],
-      :cream      => [],
-      :ice        => [],
-      :vegetable  => [],
-      :dessert    => [],
-      :grain      => [],
-      :baking     => [],
-      :meat       => [],
-      :seafood    => [],
-      :egg        => [],
-      :sweat      => [],
-      :seasoning  => [],
-      :nutritious => [],
-      :drink      => [],
-      :snack      => [],
-      :meal       => [],
-      :processed  => []
-    }
-    CSV.foreach('ref/base.csv').with_index do |row, index|
-      case index
-      when 0..16
-        base_data[:milk] << split_by_ages(row)
-      when 17..27
-        base_data[:cream] << split_by_ages(row)
-      when 28..32
-        base_data[:ice] << split_by_ages(row)
-      when 33..77
-        base_data[:vegetable] << split_by_ages(row)
-      when 78..90
-        base_data[:dessert] << split_by_ages(row)
-      when 91..127
-        base_data[:grain] << split_by_ages(row)
-      when 128..140
-        base_data[:baking] << split_by_ages(row)
-      when 141..172
-        base_data[:meat] << split_by_ages(row)
-      when 173..202
-        base_data[:seafood] << split_by_ages(row)
-      when 203..210
-        base_data[:egg] << split_by_ages(row)
-      when 211..224
-        base_data[:sweat] << split_by_ages(row)
-      when 225..250
-        base_data[:seasoning] << split_by_ages(row)
-      when 251..259
-        base_data[:nutritious] << split_by_ages(row)
-      when 260..279
-        base_data[:drink] << split_by_ages(row)
-      when 280..282
-        base_data[:snack] << split_by_ages(row)
-      when 283
-        base_data[:meal] << split_by_ages(row)
-      when 284
-        base_data[:processed] << split_by_ages(row)
+  FOOD_REF = {
+    :milk       => (0..16),
+    :cream      => (17..27),
+    :ice        => (28..32),
+    :vegetable  => (33..77),
+    :dessert    => (78..90),
+    :grain      => (91..127),
+    :baking     => (128..140),
+    :meat       => (141..172),
+    :seafood    => (173..202),
+    :egg        => (203..210),
+    :sweat      => (211..224),
+    :seasoning  => (225..250),
+    :nutritious => (251..259),
+    :drink      => (260..279),
+    :snack      => (280..282),
+    :meal       => (283..283),
+    :processed  => (284..284)
+  }
+
+  def self.intake
+    intake = {}
+    raw_intake = CSV.read('ref/base.csv', :headers=>true)
+    raw_intake.headers.each do |age_class|
+      age = age_class.to_sym
+      # init_intake(intake, age)
+      intake[age] ||= {}
+      FOOD_REF.each do |food, indexs|
+        intake[age][food] ||= raw_intake[age_class][indexs].map!(&:to_f)
       end
     end
-    base_data
+    intake
   end
 
-  def self.split_by_ages(row)
-    row.map!(&:to_f)
-    return {
-      :kid      => row[0..1],
-      :child    => row[2..7],
-      :teenager => row[8..13],
-      :adult    => row[13..18],
-      :older    => row[19..24]
-    }
+  def self.init_intake(intake, age)
+    intake[age] ||= {}
+    FOOD_REF.each_key do |food|
+      intake[age][food] ||= []
+    end
   end
 end
